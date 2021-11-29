81a172bb2bdf3ab7feadaf1f0bf6e605a7fb9cfc
Noamstrauss <87988633+Noamstrauss@users.noreply.github.com>
2021-11-29 19:33:48 +0200

    Added tf terraform file

--- infra/main.tf
+++ infra/main.tf
@@ -17,6 +17,7 @@ provider "aws" {
 resource "aws_instance" "Terraform-Test-ec2" {
   ami           = "ami-830c94e3"
   instance_type = "t2.micro"
+  
 
   tags = {
     Name = "ExampleAppServerInstance"
