8e9370b8353da31e0560461579d268679b8c3bc4
Noamstrauss <87988633+Noamstrauss@users.noreply.github.com>
2022-04-16 18:49:29 +0300

    testing outputs.tf

--- infra/youtube-frontend/output.tf
+++ infra/youtube-frontend/output.tf
@@ -9,3 +9,7 @@
 #output "identity-oidc-issuer" {
 #  value = data.aws_eks_cluster.cluster.identity[0].oidc[0].issuer
 #}
+
+output "load_balancer_hostname" {
+  value = kubernetes_ingress_v1.frontend.status.0.load_balancer.0.ingress.0.hostname
+}
