154769502c550dc6e44b445c89cf9a4409c2ce72
Noamstrauss <87988633+Noamstrauss@users.noreply.github.com>
2022-05-08 23:23:53 +0300

    trying to fix import config in get_users_tags.py

--- backend/get_users_tags.py
+++ backend/get_users_tags.py
@@ -1,7 +1,7 @@
 import boto3
 from termcolor import colored
 from loguru import logger
-from config.config import *
+from config.config import admin
 client = boto3.client('iam')
 active = False
 response = client.list_users()
